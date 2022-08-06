6e95291830b6fc70cf0a6a058d65e33d4a0a729a
Aditya Mandiyal <mandiyaladitya@gmail.com>
2022-08-06 16:33:38 +0530

    Version 1.1 (constructor created)

--- SalaryIncomeCalculator.java
+++ SalaryIncomeCalculator.java
@@ -1,9 +1,32 @@
 package com.godigit.banking;
 
+ class Salary{
+	private double basicSalary;
+	private double hra;
+	private double da;
+	
+	
+	// constructor for salary , we don't accept salary less than 1
+	public Salary(double basicSalary)
+	{
+	     if(basicSalary<1)
+	     {
+	    	 System.out.println("Sorry You Can't enter Salary less than 1 !!!!!"+"\n Thanks");
+	     }
+	     else
+	     {
+	    	 this.basicSalary=basicSalary;
+	     }
+	}
+	
+}
 public class SalaryIncomeCalculator {
-
+    
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
